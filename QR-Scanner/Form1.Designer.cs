896808edace20404fa3d3b4f0f134a3c272a9e3c
Dmitry Seliundiaev <Dmitry Seliundiaev@DESKTOP-MKURO6S>
2022-06-08 16:11:32 +0300

    Добавьте файлы проекта.

---
+++ QR-Scanner/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace QR_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxOfAllCodes = new System.Windows.Forms.ListBox();
+            this.BtnAddItem = new System.Windows.Forms.Button();
+            this.BtnDeleteItem = new System.Windows.Forms.Button();
+            this.BtnSaveQR = new System.Windows.Forms.Button();
+            this.TextBoxNewItemName = new System.Windows.Forms.TextBox();
+            this.pictureBoxWebCam = new System.Windows.Forms.PictureBox();
+            this.BtnScanning = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ListBoxOfAllCodes
+            // 
+            this.ListBoxOfAllCodes.FormattingEnabled = true;
+            this.ListBoxOfAllCodes.ItemHeight = 16;
+            this.ListBoxOfAllCodes.Location = new System.Drawing.Point(51, 126);
+            this.ListBoxOfAllCodes.Name = "ListBoxOfAllCodes";
+            this.ListBoxOfAllCodes.Size = new System.Drawing.Size(198, 260);
+            this.ListBoxOfAllCodes.TabIndex = 0;
+            // 
+            // BtnAddItem
+            // 
+            this.BtnAddItem.Location = new System.Drawing.Point(51, 41);
+            this.BtnAddItem.Name = "BtnAddItem";
+            this.BtnAddItem.Size = new System.Drawing.Size(94, 51);
+            this.BtnAddItem.TabIndex = 1;
+            this.BtnAddItem.Text = "Добавить";
+            this.BtnAddItem.UseVisualStyleBackColor = true;
+            this.BtnAddItem.Click += new System.EventHandler(this.BtnAddItem_Click);
+            // 
+            // BtnDeleteItem
+            // 
+            this.BtnDeleteItem.Location = new System.Drawing.Point(151, 41);
+            this.BtnDeleteItem.Name = "BtnDeleteItem";
+            this.BtnDeleteItem.Size = new System.Drawing.Size(98, 51);
+            this.BtnDeleteItem.TabIndex = 2;
+            this.BtnDeleteItem.Text = "Удалить";
+            this.BtnDeleteItem.UseVisualStyleBackColor = true;
+            this.BtnDeleteItem.Click += new System.EventHandler(this.BtnDeleteItem_Click);
+            // 
+            // BtnSaveQR
+            // 
+            this.BtnSaveQR.Location = new System.Drawing.Point(51, 392);
+            this.BtnSaveQR.Name = "BtnSaveQR";
+            this.BtnSaveQR.Size = new System.Drawing.Size(198, 57);
+            this.BtnSaveQR.TabIndex = 3;
+            this.BtnSaveQR.Text = "Сохранить QR-CODE";
+            this.BtnSaveQR.UseVisualStyleBackColor = true;
+            this.BtnSaveQR.Click += new System.EventHandler(this.BtnSaveQR_Click);
+            // 
+            // TextBoxNewItemName
+            // 
+            this.TextBoxNewItemName.Location = new System.Drawing.Point(51, 98);
+            this.TextBoxNewItemName.Name = "TextBoxNewItemName";
+            this.TextBoxNewItemName.Size = new System.Drawing.Size(198, 22);
+            this.TextBoxNewItemName.TabIndex = 4;
+            // 
+            // pictureBoxWebCam
+            // 
+            this.pictureBoxWebCam.Location = new System.Drawing.Point(296, 12);
+            this.pictureBoxWebCam.Name = "pictureBoxWebCam";
+            this.pictureBoxWebCam.Size = new System.Drawing.Size(775, 590);
+            this.pictureBoxWebCam.TabIndex = 5;
+            this.pictureBoxWebCam.TabStop = false;
+            // 
+            // BtnScanning
+            // 
+            this.BtnScanning.Location = new System.Drawing.Point(92, 545);
+            this.BtnScanning.Name = "BtnScanning";
+            this.BtnScanning.Size = new System.Drawing.Size(198, 57);
+            this.BtnScanning.TabIndex = 6;
+            this.BtnScanning.Text = "Прочитать код";
+            this.BtnScanning.UseVisualStyleBackColor = true;
+            this.BtnScanning.Click += new System.EventHandler(this.BtnScanning_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 640);
+            this.Controls.Add(this.BtnScanning);
+            this.Controls.Add(this.pictureBoxWebCam);
+            this.Controls.Add(this.TextBoxNewItemName);
+            this.Controls.Add(this.BtnSaveQR);
+            this.Controls.Add(this.BtnDeleteItem);
+            this.Controls.Add(this.BtnAddItem);
+            this.Controls.Add(this.ListBoxOfAllCodes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxOfAllCodes;
+        private System.Windows.Forms.Button BtnAddItem;
+        private System.Windows.Forms.Button BtnDeleteItem;
+        private System.Windows.Forms.Button BtnSaveQR;
+        private System.Windows.Forms.TextBox TextBoxNewItemName;
+        private System.Windows.Forms.PictureBox pictureBoxWebCam;
+        private System.Windows.Forms.Button BtnScanning;
+    }
+}
+
---
+++ QR-Scanner/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace QR_Scanner
+{
+    public partial class Form1 : Form
+    {
+        public List<string> Numbers = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            SaveRecover();
+            VideoStart();
+        }
+        FilterInfoCollection FilterInfoCollection;
+        VideoCaptureDevice VideoCaptureDevice;
+        private void SaveData()
+        {
+            System.IO.Stream ms = File.OpenWrite(@"AutoSave");
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (BinaryWriter bw = new BinaryWriter(ms))
+            {
+                formatter.Serialize(ms, Numbers);
+            }
+        }
+        private void SaveRecover()
+        {
+            if (File.Exists(@"AutoSave"))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                using (FileStream fs = File.Open(@"AutoSave", FileMode.Open))
+                {
+                    Numbers = (List<string>)formatter.Deserialize(fs);
+                }
+                foreach (var item in Numbers)
+                {
+                    ListBoxOfAllCodes.Items.Add(item);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void VideoStart()
+        {
+            FilterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice = new VideoCaptureDevice(FilterInfoCollection[0].MonikerString);
+            VideoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            VideoCaptureDevice.Start();
+        }
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureBoxWebCam.Image = (Bitmap)eventArgs.Frame.Clone();
+            Scanning(eventArgs);
+        }
+
+        private void BtnAddItem_Click(object sender, EventArgs e)
+        {
+            ListBoxOfAllCodes.Items.Add(TextBoxNewItemName.Text);
+            Numbers.Add(TextBoxNewItemName.Text);
+            SaveData();
+        }
+
+        private void BtnDeleteItem_Click(object sender, EventArgs e)
+        {
+            Numbers.Remove(ListBoxOfAllCodes.SelectedItem.ToString());
+            ListBoxOfAllCodes.Items.Remove(ListBoxOfAllCodes.SelectedItem);
+            SaveData();
+        }
+
+        private void BtnSaveQR_Click(object sender, EventArgs e)
+        {
+            var data = ListBoxOfAllCodes.SelectedItem.ToString();
+            var path = $@"D:\\{data}.Bmp";
+            BarcodeWriter writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Write(data).Save(path);
+        }
+
+        private string scannedText;
+        private void Scanning(NewFrameEventArgs eventArgs)
+        {
+            BarcodeReader reader = new BarcodeReader();
+            var result = reader.Decode((Bitmap)eventArgs.Frame.Clone());
+            if (result != null)
+            {
+                scannedText = result.Text;
+
+            }
+
+        }
+
+        private void BtnScanning_Click_1(object sender, EventArgs e)
+        {
+            if (scannedText != null)
+            {
+                ListBoxOfAllCodes.SelectedItem = scannedText;
+            }
+            scannedText = null;
+        }
+    }
+}
