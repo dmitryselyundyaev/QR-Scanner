f1363673fd5cc02d9fe621e8e137032d5c1378e5
Dmitry Selyundyaev <dmitry.selyundyaev@gmail.com>
2022-06-09 14:59:15 +0300

    Final

--- QR-Scanner/Form1.Designer.cs
+++ QR-Scanner/Form1.Designer.cs
@@ -35,6 +35,9 @@
             this.TextBoxNewItemName = new System.Windows.Forms.TextBox();
             this.pictureBoxWebCam = new System.Windows.Forms.PictureBox();
             this.BtnScanning = new System.Windows.Forms.Button();
+            this.BtnPath = new System.Windows.Forms.Button();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelScanned = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxWebCam)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,34 +45,34 @@
             // 
             this.ListBoxOfAllCodes.FormattingEnabled = true;
             this.ListBoxOfAllCodes.ItemHeight = 16;
-            this.ListBoxOfAllCodes.Location = new System.Drawing.Point(51, 126);
+            this.ListBoxOfAllCodes.Location = new System.Drawing.Point(12, 158);
             this.ListBoxOfAllCodes.Name = "ListBoxOfAllCodes";
             this.ListBoxOfAllCodes.Size = new System.Drawing.Size(198, 260);
             this.ListBoxOfAllCodes.TabIndex = 0;
             // 
             // BtnAddItem
             // 
-            this.BtnAddItem.Location = new System.Drawing.Point(51, 41);
+            this.BtnAddItem.Location = new System.Drawing.Point(12, 73);
             this.BtnAddItem.Name = "BtnAddItem";
             this.BtnAddItem.Size = new System.Drawing.Size(94, 51);
             this.BtnAddItem.TabIndex = 1;
             this.BtnAddItem.Text = "Добавить";
             this.BtnAddItem.UseVisualStyleBackColor = true;
-            this.BtnAddItem.Click += new System.EventHandler(this.BtnAddItem_Click);
+            this.BtnAddItem.Visible = false;
             // 
             // BtnDeleteItem
             // 
-            this.BtnDeleteItem.Location = new System.Drawing.Point(151, 41);
+            this.BtnDeleteItem.Location = new System.Drawing.Point(112, 73);
             this.BtnDeleteItem.Name = "BtnDeleteItem";
             this.BtnDeleteItem.Size = new System.Drawing.Size(98, 51);
             this.BtnDeleteItem.TabIndex = 2;
             this.BtnDeleteItem.Text = "Удалить";
             this.BtnDeleteItem.UseVisualStyleBackColor = true;
-            this.BtnDeleteItem.Click += new System.EventHandler(this.BtnDeleteItem_Click);
+            this.BtnDeleteItem.Visible = false;
             // 
             // BtnSaveQR
             // 
-            this.BtnSaveQR.Location = new System.Drawing.Point(51, 392);
+            this.BtnSaveQR.Location = new System.Drawing.Point(12, 424);
             this.BtnSaveQR.Name = "BtnSaveQR";
             this.BtnSaveQR.Size = new System.Drawing.Size(198, 57);
             this.BtnSaveQR.TabIndex = 3;
@@ -79,22 +82,23 @@
             // 
             // TextBoxNewItemName
             // 
-            this.TextBoxNewItemName.Location = new System.Drawing.Point(51, 98);
+            this.TextBoxNewItemName.Location = new System.Drawing.Point(12, 130);
             this.TextBoxNewItemName.Name = "TextBoxNewItemName";
             this.TextBoxNewItemName.Size = new System.Drawing.Size(198, 22);
             this.TextBoxNewItemName.TabIndex = 4;
+            this.TextBoxNewItemName.Visible = false;
             // 
             // pictureBoxWebCam
             // 
-            this.pictureBoxWebCam.Location = new System.Drawing.Point(296, 12);
+            this.pictureBoxWebCam.Location = new System.Drawing.Point(238, 60);
             this.pictureBoxWebCam.Name = "pictureBoxWebCam";
-            this.pictureBoxWebCam.Size = new System.Drawing.Size(775, 590);
+            this.pictureBoxWebCam.Size = new System.Drawing.Size(1030, 611);
             this.pictureBoxWebCam.TabIndex = 5;
             this.pictureBoxWebCam.TabStop = false;
             // 
             // BtnScanning
             // 
-            this.BtnScanning.Location = new System.Drawing.Point(92, 545);
+            this.BtnScanning.Location = new System.Drawing.Point(12, 487);
             this.BtnScanning.Name = "BtnScanning";
             this.BtnScanning.Size = new System.Drawing.Size(198, 57);
             this.BtnScanning.TabIndex = 6;
@@ -102,11 +106,43 @@
             this.BtnScanning.UseVisualStyleBackColor = true;
             this.BtnScanning.Click += new System.EventHandler(this.BtnScanning_Click_1);
             // 
+            // BtnPath
+            // 
+            this.BtnPath.Location = new System.Drawing.Point(12, 12);
+            this.BtnPath.Name = "BtnPath";
+            this.BtnPath.Size = new System.Drawing.Size(128, 51);
+            this.BtnPath.TabIndex = 7;
+            this.BtnPath.Text = "Путь к файлу";
+            this.BtnPath.UseVisualStyleBackColor = true;
+            this.BtnPath.Click += new System.EventHandler(this.BtnPath_Click);
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPath.Location = new System.Drawing.Point(146, 19);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(136, 18);
+            this.labelPath.TabIndex = 8;
+            this.labelPath.Text = "Файл не выбран";
+            // 
+            // labelScanned
+            // 
+            this.labelScanned.AutoSize = true;
+            this.labelScanned.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelScanned.Location = new System.Drawing.Point(587, 22);
+            this.labelScanned.Name = "labelScanned";
+            this.labelScanned.Size = new System.Drawing.Size(0, 25);
+            this.labelScanned.TabIndex = 9;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 640);
+            this.ClientSize = new System.Drawing.Size(1302, 721);
+            this.Controls.Add(this.labelScanned);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.BtnPath);
             this.Controls.Add(this.BtnScanning);
             this.Controls.Add(this.pictureBoxWebCam);
             this.Controls.Add(this.TextBoxNewItemName);
@@ -131,6 +167,9 @@
         private System.Windows.Forms.TextBox TextBoxNewItemName;
         private System.Windows.Forms.PictureBox pictureBoxWebCam;
         private System.Windows.Forms.Button BtnScanning;
+        private System.Windows.Forms.Button BtnPath;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Label labelScanned;
     }
 }
 
--- QR-Scanner/Form1.cs
+++ QR-Scanner/Form1.cs
@@ -1,18 +1,12 @@
-﻿using System;
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using Spire.Doc;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZXing;
-using AForge;
-using AForge.Video;
-using AForge.Video.DirectShow;
 
 namespace QR_Scanner
 {
@@ -22,40 +16,17 @@ namespace QR_Scanner
         public Form1()
         {
             InitializeComponent();
-            SaveRecover();
+            //SaveRecover();
             VideoStart();
         }
         FilterInfoCollection FilterInfoCollection;
         VideoCaptureDevice VideoCaptureDevice;
-        private void SaveData()
-        {
-            System.IO.Stream ms = File.OpenWrite(@"AutoSave");
-            BinaryFormatter formatter = new BinaryFormatter();
-            using (BinaryWriter bw = new BinaryWriter(ms))
-            {
-                formatter.Serialize(ms, Numbers);
-            }
-        }
-        private void SaveRecover()
-        {
-            if (File.Exists(@"AutoSave"))
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                using (FileStream fs = File.Open(@"AutoSave", FileMode.Open))
-                {
-                    Numbers = (List<string>)formatter.Deserialize(fs);
-                }
-                foreach (var item in Numbers)
-                {
-                    ListBoxOfAllCodes.Items.Add(item);
-                }
-            }
-        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
+        ///Initializing video device and creating handler
         public void VideoStart()
         {
             FilterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
@@ -63,35 +34,24 @@ namespace QR_Scanner
             VideoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
             VideoCaptureDevice.Start();
         }
+        ///Handler for picture box updating
         private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             pictureBoxWebCam.Image = (Bitmap)eventArgs.Frame.Clone();
             Scanning(eventArgs);
         }
 
-        private void BtnAddItem_Click(object sender, EventArgs e)
-        {
-            ListBoxOfAllCodes.Items.Add(TextBoxNewItemName.Text);
-            Numbers.Add(TextBoxNewItemName.Text);
-            SaveData();
-        }
-
-        private void BtnDeleteItem_Click(object sender, EventArgs e)
-        {
-            Numbers.Remove(ListBoxOfAllCodes.SelectedItem.ToString());
-            ListBoxOfAllCodes.Items.Remove(ListBoxOfAllCodes.SelectedItem);
-            SaveData();
-        }
-
+        ///Save and generate QR.
         private void BtnSaveQR_Click(object sender, EventArgs e)
         {
             var data = ListBoxOfAllCodes.SelectedItem.ToString();
-            var path = $@"D:\\{data}.Bmp";
+            var path = $@"C:\\Users\\Xeon\\Desktop\\{ListBoxOfAllCodes.SelectedIndex}.Bmp";
             BarcodeWriter writer = new BarcodeWriter();
             writer.Format = BarcodeFormat.QR_CODE;
             writer.Write(data).Save(path);
         }
 
+        ///Method decode bitmap and updating scanned QR.
         private string scannedText;
         private void Scanning(NewFrameEventArgs eventArgs)
         {
@@ -104,14 +64,45 @@ namespace QR_Scanner
             }
 
         }
-
+        ///Scanning button.
         private void BtnScanning_Click_1(object sender, EventArgs e)
         {
             if (scannedText != null)
             {
                 ListBoxOfAllCodes.SelectedItem = scannedText;
+                labelScanned.Text = scannedText;
             }
             scannedText = null;
+        }
+
+        ///Filepath.
+        public string filePath;
+        public Document doc;
+        private void BtnPath_Click(object sender, EventArgs e)
+        {
+            Numbers = null;
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = ofd.FileName;
+                labelPath.Text = ofd.FileName;
+            }
+            if (filePath != null)
+            {
+                //Create a Document object
+                doc = new Document();
+                //Load a Word file
+                doc.LoadFromFile(filePath);
+                var text = doc.GetText();
+                Numbers = text.Split(',').ToList();
+                foreach (var number in Numbers)
+                {
+                    if (number.Length < 20)
+                    {
+                        ListBoxOfAllCodes.Items.Add(number);
+                    }
+                }
+            }
         }
     }
 }
